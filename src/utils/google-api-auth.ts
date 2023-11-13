5f5fb2dddfacddc11f95c449c0a96b8608363bbd
Juan Angel Sarmiento Rodriguez <jasr4075@gmail.com>
2023-11-13 17:01:19 -0300

    codigo de google-api-auth optimizado

--- src/utils/google-api-auth.ts
+++ src/utils/google-api-auth.ts
@@ -9,11 +9,14 @@ const AUTH_SCOPES = [
   "https://www.googleapis.com/auth/userinfo.email",
 ];
 
-export const googleCredentials = {
-  client_id: process.env.GOOGLE_CLIENT_ID || 'default_client_id',
-  client_secret: process.env.GOOGLE_CLIENT_SECRET || 'default_client_secret',
-};
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error('The GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET environment variables are not defined');
+}
 
+export const googleCredentials = {
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  client_secret: process.env.GOOGLE_CLIENT_SECRET,
+};
 
 export async function getAuthUrl(oauth2Client: Auth.OAuth2Client) {
   const authUrl = oauth2Client.generateAuthUrl({
@@ -24,62 +27,53 @@ export async function getAuthUrl(oauth2Client: Auth.OAuth2Client) {
 }
 
 export async function makeOAuth2Client(keys: { client_id: string, client_secret: string }) {
-  return new google.auth.OAuth2({
-    clientId: keys.client_id,
-    clientSecret: keys.client_secret,
-    redirectUri: REDIRECT_URI,
-  });
+  return createOAuth2Client(keys);
 }
 
 export async function exchangeCodeForToken(code: string) {
   const oauth2Client = await makeOAuth2Client(googleCredentials);
-  try {
-    const { tokens } = await oauth2Client.getToken(code);
-    console.log(tokens);
-    return tokens;
-  } catch (error) {
-    throw error;
-  }
+  const { tokens } = await oauth2Client.getToken(code);
+  console.log(tokens);
+  return tokens;
 }
 
 export async function getUserInfo(accessToken: string, refreshToken: string) {
-  const oauth2Client = new google.auth.OAuth2();
+  const oauth2Client = createOAuth2Client();
   oauth2Client.setCredentials({
     access_token: accessToken,
     refresh_token: refreshToken,
   });
   const people = google.people({ version: 'v1', auth: oauth2Client });
-  try {
-    const userInfoResponse = await people.people.get({
-      resourceName: 'people/me',
-      personFields: 'names,emailAddresses,phoneNumbers',
-    });
-    const userInfo = userInfoResponse.data;
-    console.log(userInfo);
-    const token = refreshToken;
-    const displayName = userInfo.names ? userInfo.names[0]?.displayName : '';
-    const emailAddress = userInfo.emailAddresses ? userInfo.emailAddresses[0]?.value : '';
-    const phoneNumber = userInfo.phoneNumbers ? userInfo.phoneNumbers[0]?.value : '';
-    const user = {
-      email: emailAddress || '',
-      name: displayName,
-      phone: phoneNumber || '',
-      role: 'customer',
-      token,
-      taxIdentificationNumber: '',
-    };
-    console.log(user);
-    return user;
-  } catch (error) {
-    console.error('Error getting user information', error);
-    throw error;
-  }
+  const userInfoResponse = await people.people.get({
+    resourceName: 'people/me',
+    personFields: 'names,emailAddresses,phoneNumbers',
+  });
+  const userInfo = userInfoResponse.data;
+  console.log(userInfo);
+  const token = refreshToken;
+  const displayName = userInfo.names ? userInfo.names[0]?.displayName : '';
+  const emailAddress = userInfo.emailAddresses ? userInfo.emailAddresses[0]?.value : '';
+  const phoneNumber = userInfo.phoneNumbers ? userInfo.phoneNumbers[0]?.value : '';
+  const user = {
+    email: emailAddress || '',
+    name: displayName,
+    phone: phoneNumber || '',
+    role: 'customer',
+    token,
+    taxIdentificationNumber: '',
+  };
+  console.log(user);
+  return user;
 }
 
 export async function insertUserInfo(user: Users) {
-  try {
-    const newUser = await Users.create(user);
-  } catch (error) {
-    throw error;
-  }
+  const newUser = await Users.create(user);
+}
+
+function createOAuth2Client(keys?: { client_id: string, client_secret: string }) {
+  return new google.auth.OAuth2({
+    clientId: keys?.client_id,
+    clientSecret: keys?.client_secret,
+    redirectUri: REDIRECT_URI,
+  });
 }
